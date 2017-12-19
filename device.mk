5d97b6edf282304b2ce66a7f46ef99f90f982ecb
Adesh15 <adesikha15@gmail.com>
2017-12-19 19:10:27 +0100

    Cheeseburger:Add config.fs filesystem

--- device.mk
+++ device.mk
@@ -227,10 +227,6 @@ PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
     android.hardware.gatekeeper@1.0-service
 
-# For android_filesystem_config.h
-PRODUCT_PACKAGES += \
-    fs_config_files
-
 # GPS
 PRODUCT_PACKAGES += \
     gps.msm8998 \
