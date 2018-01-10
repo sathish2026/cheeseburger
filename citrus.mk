ac5e201830e0fec0f257189df3375bb533087e5d
sathish2026 <txt2sathish@gmail.com>
2018-01-11 00:29:17 +0530

    cheeseburger: citrus rebrand
    Signed-off-by: sathish2026 <txt2sathish@gmail.com>

--- citrus.mk
+++ citrus.mk
@@ -22,9 +22,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/oneplus/cheeseburger/device.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cardinal/common.mk)
+$(call inherit-product, vendor/citrus/common.mk)
 
-PRODUCT_NAME := cardinal_cheeseburger
+PRODUCT_NAME := citrus_cheeseburger
 PRODUCT_DEVICE := cheeseburger
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
