ed566e78102ad1d8ada8c4bc4ddde7ffc8c01565
sathish2026 <txt2sathish@gmail.com>
2018-01-13 15:32:19 +0530

    cheeseburger: remove unwanted packages

--- device.mk
+++ device.mk
@@ -205,8 +205,6 @@ PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.1-service \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
-    vendor.display.color@1.0-service \
-    vendor.display.color@1.0-impl \
     vendor.display.config@1.0 \
     vendor.display.config@1.0_vendor
 
