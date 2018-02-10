8f294e57cd5c11ba4fef4cfa8afcb093a415be4b
sathish2026 <txt2sathish@gmail.com>
2018-02-10 21:23:24 +0530

    oneplus5: build missing camera hidl

--- hidl.mk
+++ hidl.mk
@@ -17,7 +17,8 @@ PRODUCT_PACKAGES += android.hardware.camera.provider@2.4-impl
 #PRODUCT_PACKAGES += android.hardware.camera.provider@2.4-service
 
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.camera.device@1.0
+    vendor.qti.hardware.camera.device@1.0 \
+    vendor.qti.hardware.camera.device@1.0_vendor
 
 # Display/Graphics
 PRODUCT_PACKAGES += \
